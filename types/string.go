2d2e62a69db0c78a635cf7762d958fbb368ce5aa
Nicolas S. Dade <nic.dade@gmail.com>
2022-11-01 22:09:17 -0700

    handle strings needing escape slightly more efficiently
    by figuring out they need escape during NewString(), which is
    when they are hot in the cache, but delaying the escaping until
    the string is written.
    
    the fact that this uses two different types means that casting to
    types.String isn't enough anymore.

--- types/string.go
+++ types/string.go
@@ -5,69 +5,67 @@
 package types
 
 import (
-	"bytes"
 	"strings"
 )
 
 type String []byte
+type StringNeedingEscaping []byte
 
-func NewString(s []byte) String {
+func NewString(s []byte) Object {
 	// do we need to escape this string?
 	for _, r := range s {
 		if r < 0x20 || r == '"' || r == '\\' || r >= 0x80 {
-			return escapedString(s)
+			return StringNeedingEscaping(s)
 		}
 	}
-
-	// we only need quotes around the string
+	// the entire string is escape-free (a common case)
 	return String(s)
 }
 
-func escapedString(s []byte) String {
+func (s StringNeedingEscaping) JSON(b *strings.Builder) {
+	b.WriteByte('"')
 	// the rule in JSON in that JSON text must be UTF-8, or if you must, use unicode \uxxxx notation.
 	// only ascii control chars (<0x20), \ and " need to be escaped, and some control chars can use \[bfnrt/] instead of \u00xx encoding.
-	// (why \/ might be used I don't know, but it's there on json.org's flow chart
-	// We used to use "%q" printf formatting, but it's a huge hot spot (30% of CPU time) since every single field name gets encoded this way
-	var buf = bytes.NewBuffer(make([]byte, 0, len(s)*2)) // *2 is a rough guess
+	// (why / might need to be escaped I don't know, but it's there on json.org's flow chart. The code in stdlib doesn't escaping it, so I won't either)
 	for _, r := range string(s) {
 		switch r {
 		case '"':
-			buf.WriteByte('\\')
-			buf.WriteByte('"')
+			b.WriteByte('\\')
+			b.WriteByte('"')
 		case '\\':
-			buf.WriteByte('\\')
-			buf.WriteByte('\\')
+			b.WriteByte('\\')
+			b.WriteByte('\\')
 		case '\u2028':
-			buf.WriteString("\\u2028")
+			b.WriteString("\\u2028")
 		case '\u2029':
-			buf.WriteString("\\u2029")
+			b.WriteString("\\u2029")
 		case '\b':
-			buf.WriteByte('\\')
-			buf.WriteByte('b')
+			b.WriteByte('\\')
+			b.WriteByte('b')
 		case '\f':
-			buf.WriteByte('\\')
-			buf.WriteByte('f')
+			b.WriteByte('\\')
+			b.WriteByte('f')
 		case '\n':
-			buf.WriteByte('\\')
-			buf.WriteByte('n')
+			b.WriteByte('\\')
+			b.WriteByte('n')
 		case '\r':
-			buf.WriteByte('\\')
-			buf.WriteByte('r')
+			b.WriteByte('\\')
+			b.WriteByte('r')
 		case '\t':
-			buf.WriteByte('\\')
-			buf.WriteByte('t')
+			b.WriteByte('\\')
+			b.WriteByte('t')
 		default:
 			if r < 0x20 {
-				buf.WriteString("\\u00")
-				buf.WriteByte('0' + byte(r>>4))
-				buf.WriteByte(hex[r&0xf])
+				b.WriteString("\\u00")
+				b.WriteByte('0' + byte(r>>4))
+				b.WriteByte(hex[r&0xf])
 			} else {
 				// all other runes, even those >= 0x80, can be themselves. JSON source is UTF-8
-				buf.WriteRune(r)
+				b.WriteRune(r)
 			}
 		}
 	}
-	return String(buf.Bytes())
+	b.WriteByte('"')
 }
 
 const hex = "0123456789abcdef"
@@ -82,5 +80,4 @@ func (s String) JSON(b *strings.Builder) {
 // return the unescaped string (useful every once in a while)
 func (s String) String() string {
 	return string(s)
-	// NOTE: in git history is an un-escaper, should that ever be useful again
 }
